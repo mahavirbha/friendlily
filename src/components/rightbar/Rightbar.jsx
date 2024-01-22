44c58d03efae9696fc84254e60e80992cb2311d2
mahavir-amoga <mahavirsinh.chauhan@amoga.io>
2024-01-23 02:49:07 +0530

    Amoga Ads addd 2e

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,88 +1,88 @@
 import './rightbar.css';
-import {gift,ad} from '../../assets/images'
-import { person1, person2,person3,person4,person5,person6,person7,person8,person9,person10} from '../../assets/images';
+import { gift, ad } from '../../assets/images'
+import { person1, person2, person3, person4, person5, person6, person7, person8, person9, person10 } from '../../assets/images';
 import { Users } from '../../dummyData';
 import Online from '../online/Online';
 
 
-export default function Rightbar({profile}) {
+export default function Rightbar({ profile }) {
 
-  const HomeRightbar = () => {
-    return(
-      <>
-        <div className="birthdayContainer">
-          <img src={gift} alt="gift img" className="birthdayImg" />
-          <span className="birthdayText"><b>Divyesh Varu</b> and <b>3 other friends</b> have a birthday today.</span>
-        </div>
-        <img src={ad} alt="Ad img" className="rightbarAd" />
-        <div className="rightbarOnlineContainer">
+    const HomeRightbar = () => {
+        return (
+            <>
+                <div className="birthdayContainer">
+                    <img src={gift} alt="gift img" className="birthdayImg" />
+                    <span className="birthdayText"><b>Divyesh Varu</b> and <b>3 other friends</b> have a birthday today.</span>
+                </div>
+                <img src={ad} alt="Ad img" className="rightbarAd" />
+                <div className="rightbarOnlineContainer">
 
-          <h4 className="rightbarTitle">Online Friends</h4>
+                    <h4 className="rightbarTitle">Online Friends</h4>
 
-        <ul className="rightbarFriendList">
-          {Users.map(u=>(
-            <Online key={u.id} user={u} />
-          ))}
-        </ul>
+                    <ul className="rightbarFriendList">
+                        {Users?.filter((user) => user.id != 12).map(u => (
+                            <Online key={u.id} user={u} />
+                        ))}
+                    </ul>
 
-        </div>
-      </>
-    );
-  };
+                </div>
+            </>
+        );
+    };
 
 
-  const ProfileRightbar = () => {
-    return (
-      <>
-        <h4 className="rightbarTitle">User information</h4>
-        <div className="rightbarInfo">
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">Himmatnagar</span>
-          </div>
+    const ProfileRightbar = () => {
+        return (
+            <>
+                <h4 className="rightbarTitle">User information</h4>
+                <div className="rightbarInfo">
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">City:</span>
+                        <span className="rightbarInfoValue">Himmatnagar</span>
+                    </div>
 
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">Bayad</span>
-          </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">From:</span>
+                        <span className="rightbarInfoValue">Bayad</span>
+                    </div>
 
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">Single</span>
-          </div>
-          
-        </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Relationship:</span>
+                        <span className="rightbarInfoValue">Single</span>
+                    </div>
 
-        <h4 className="rightbarTitle">Friends</h4>
-        <div className="rightbarFollowings">
+                </div>
 
-          {Users.map(u=>(
-              <div className='rightbarFollowing'>
-              <img src={u.profilePicture} alt="" className="rightbarFollowingImg" />
-              <span className="rightbarFollowingName">{u.username}</span>
-              </div>
-          ))}
+                <h4 className="rightbarTitle">Friends</h4>
+                <div className="rightbarFollowings">
 
-          {/* <div className="rightbarFollowing">
+                    {Users?.filter((user) => user.id != 12).map(u => (
+                        <div className='rightbarFollowing'>
+                            <img src={u.profilePicture} alt="" className="rightbarFollowingImg" />
+                            <span className="rightbarFollowingName">{u.username}</span>
+                        </div>
+                    ))}
+
+                    {/* <div className="rightbarFollowing">
             <img src={person1} alt="" className="rightbarFollowingImg" />
             <span className="rightbarFollowingName">Mahavirbha Chauhan</span>
           </div>
 
           */}
-          
+
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <div className='rightbar'>
+            <div className="rightbarWrapper">
+                {profile ? <ProfileRightbar /> : <HomeRightbar />}
+                {/* <ProfileRightbar /> */}
+                {/* <HomeRightbar /> */}
+
+            </div>
         </div>
-      </>
     );
-  }
-
-  return (
-    <div className='rightbar'>
-        <div className="rightbarWrapper">
-          {profile ? <ProfileRightbar /> : <HomeRightbar />}
-          {/* <ProfileRightbar /> */}
-          {/* <HomeRightbar /> */}
-
-        </div>
-    </div>
-  );
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,88 +8,88 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 import EventIcon from '@mui/icons-material/Event';
 import SchoolIcon from '@mui/icons-material/School';
-import { person1, person2,person3,person4,person5,person6,person7,person8,person9,person10} from '../../assets/images';
+import { person1, person2, person3, person4, person5, person6, person7, person8, person9, person10 } from '../../assets/images';
 import CloseFriend from '../closeFriend/CloseFriend';
 import { Users } from '../../dummyData';
 
 export default function Sidebar() {
-  return (<div className='sidebar'>
-      <div className="sidebarWrapper">
-        <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <RssFeedIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Feed
-            </span>
-          </li>
+    return (<div className='sidebar'>
+        <div className="sidebarWrapper">
+            <ul className="sidebarList">
+                <li className="sidebarListItem">
+                    <RssFeedIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Feed
+                    </span>
+                </li>
 
-          <li className="sidebarListItem">
-            <ChatIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Feed
-            </span>
-          </li>
+                <li className="sidebarListItem">
+                    <ChatIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Feed
+                    </span>
+                </li>
 
-          <li className="sidebarListItem">
-            <PlayCircleIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Videos
-            </span>
-          </li>
+                <li className="sidebarListItem">
+                    <PlayCircleIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Videos
+                    </span>
+                </li>
 
-          <li className="sidebarListItem">
-            <GroupIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Groups
-            </span>
-          </li>
+                <li className="sidebarListItem">
+                    <GroupIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Groups
+                    </span>
+                </li>
 
-          <li className="sidebarListItem">
-            <BookmarkIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Bookmarks
-            </span>
-          </li>
+                <li className="sidebarListItem">
+                    <BookmarkIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Bookmarks
+                    </span>
+                </li>
 
-          <li className="sidebarListItem">
-            <HelpOutlineIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Questions
-            </span>
-          </li>
+                <li className="sidebarListItem">
+                    <HelpOutlineIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Questions
+                    </span>
+                </li>
 
-          <li className="sidebarListItem">
-            <WorkOutlineIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Jobs
-            </span>
-          </li>
+                <li className="sidebarListItem">
+                    <WorkOutlineIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Jobs
+                    </span>
+                </li>
 
-          <li className="sidebarListItem">
-            <EventIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Events
-            </span>
-          </li>
+                <li className="sidebarListItem">
+                    <EventIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Events
+                    </span>
+                </li>
 
-          <li className="sidebarListItem">
-            <SchoolIcon className='sidebarIcon'/>
-            <span className="sidebarListItemText">
-              Courses
-            </span>
-          </li>
+                <li className="sidebarListItem">
+                    <SchoolIcon className='sidebarIcon' />
+                    <span className="sidebarListItemText">
+                        Courses
+                    </span>
+                </li>
 
-        </ul>
+            </ul>
 
-        <button className="sidebarButton">Show More</button>
-        <hr className='sidebarHr'/>
-        <ul className="sidebarFriendList">
+            <button className="sidebarButton">Show More</button>
+            <hr className='sidebarHr' />
+            <ul className="sidebarFriendList">
 
-          {Users.map(u=>(
-            <CloseFriend key={u.id} user={u} />
-          ))}
+                {Users?.filter((user) => user.id != 12).map(u => (
+                    <CloseFriend key={u.id} user={u} />
+                ))}
 
-        </ul>
-      </div>
-  </div>);
+            </ul>
+        </div>
+    </div>);
 }
